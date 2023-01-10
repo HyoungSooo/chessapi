ea20665b949984e329ce73777491b40de9710c3a
Hyoungsooo <aaa57403@gmail.com>
2023-01-10 15:52:32 +0900

    create evaluation api, and simple chessboard

--- api/urls.py
+++ api/urls.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 #ninja api
-from ninja import NinjaAPI, File
+from ninja import NinjaAPI, File, Schema
 from ninja.files import UploadedFile
 from django.http import JsonResponse
-from api.apiutils import find_latest_file, get_stock_fish
+from api.apiutils import find_latest_file, get_stock_fish, eval_position
 import csv
 import json
 import pandas as pd
 from .models import ChessOpening
 from stockfish import StockfishException
+import chess
 
 
 api = NinjaAPI()
@@ -74,12 +75,80 @@ def get_fen_position_by_moves(request, move:str):
   except ValueError:
     msg = {'msg': 'unvalid move'}
     return JsonResponse(msg)
-  except StockfishException:
+  except:
     msg = {'msg' : 'stockfish load error plz try later'}
     return JsonResponse(msg)
 
+class Pgn(Schema):
+  pgn:str
 
-  
+@api.post('pgntouci')
+def pgn_to_uci(request, pgn:Pgn):
+  try:
+    stockfish = get_stock_fish()
+
+    data = pgn.pgn.split(' ')
+    for i in range(len(data)):
+      if data[i].endswith('.'):
+        data[i] = None
+      
+      if i == len(data)-1:
+        if data[i].find('-'):
+          data[i] = None
+    data = list(filter(None,data))
+    board = chess.Board()
+    uci = []
+    for san in data:
+      current = board.push_san(san).uci()
+      stockfish.make_moves_from_current_position([current])
+      uci.append(current)
+
+    return ','.join(uci)
+  except ValueError:
+    msg = {'msg': 'unvalid move'}
+    return JsonResponse(msg)
+
+  except:
+    msg = {'msg' : 'stockfish load error plz try later'}
+    return JsonResponse(msg)
+
+class Uci(Schema):
+  uci:str
+
+
+@api.post('/evaluate')
+def evaluate(request, uci:Uci):
+  try:
+    res = []
+    stockfish = get_stock_fish()
+    stockfish.set_position()
+    color = ['W', 'B']
+    cnt = 0
+
+    for i in uci.uci.split(','):
+      if not stockfish.is_move_correct(i):
+        return JsonResponse({'msg' : 'unvaild move is detected'})
+    
+      best_move = stockfish.get_top_moves(1)[0]
+      stockfish.make_moves_from_current_position([i])
+
+      eval = stockfish.get_evaluation()
+
+      if best_move['Centipawn'] < 0 and eval['value'] < 0:
+        msg = eval_position(abs(best_move['Centipawn'] - eval['value']))
+      elif best_move['Centipawn'] < 0 or eval['value'] < 0:
+        msg = eval_position(abs(best_move['Centipawn']) + abs(eval['value']))
+      else:
+        msg = eval_position(abs(best_move['Centipawn'] - eval['value']))
+        
+
+      res.append({'color' : color[cnt%2], 'move' : i, 'best' : best_move, 'eval' : eval, 'msg' : msg})
+
+      cnt += 1
+
+    return JsonResponse(res, safe=False)
+  except:
+    return JsonResponse({'msg' : 'evaluation fali'})
 
 urlpatterns = [
     path('api/', api.urls),
